7b303d5c088ce12e542365bf2fbf4e14f0d8095f
Your Name <you@example.com>
2023-10-04 16:07:21 -0700

    merge master and amin

---
+++ rpc/airbnb_get_img_url.js
@@ -0,0 +1,88 @@
+//f(apt) -> list of googlemap
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path')
+
+let cache = {};
+async function getImgUrl(aptListing, idx) {
+    let origAptListing = aptListing;
+
+    function urlToFileName(url) {
+        let val = url.match(/rooms\/(\d+)/)
+
+        return val ? val[1] : ''
+      }
+      aptListing = urlToFileName(aptListing);
+      if (!aptListing ) return console.log('error shit not work ' + aptListing)
+    let url = path.resolve(__dirname, `../data/airbnb/gm/${aptListing}.json`);
+    console.log(url, aptListing)
+    //console.log(aptListing, idx);
+    //if (aptListing in cache) {return}
+    cache[aptListing] = true;
+    console.log(fs.existsSync(url), url);
+    
+    if (fs.existsSync(url)) return console.log('cached url')
+    //if (fs.existsSync(url)) return
+
+    const browser = await puppeteer.launch({ headless: true }); // Replace true with false if you want to see the browser
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.goto(origAptListing);
+
+    await page.waitForSelector('section');
+    //await page.waitForSelector('.hnwb2pb.dir.dir-ltr')
+
+      await page.waitForSelector('.l1ovpqvx.c1h5tsj6.dir.dir-ltr');
+      await page.keyboard.press('Enter')
+      await page.keyboard.press('Escape')
+      await delay(3000)
+      await page.screenshot({ path: 'example.png' });
+
+    await page.evaluate(() => {
+        window.scrollBy(0, 3107);
+        document.querySelector('html').scrollTop = 3107
+        // setTimeout(function () {
+        //     document.querySelectorAll('.hnwb2pb.dir.dir-ltr')[3].scrollIntoView()
+
+        // }, 1000)
+        console.log('scrolling')
+    });
+    await page.mouse.wheel({deltaY: 3107});
+    await page.waitForSelector('.gm-style img');
+    await  delay(1000)
+    let imgUrls = await page.$$eval('img', 
+    imgs => imgs.map(img => img.src).filter(_ => _.indexOf("maps.googleapis.com") !== -1)
+        )
+    //.filter(src => src.indexOf("maps.googleapis.com") !== -1));
+    // if (imgUrls.length == 0) {
+    //     delay(3000)
+    //     imgUrls = await page.$$eval('.gm-style img', imgs => imgs.map(img => img.src).filter(src => src.indexOf("maps.googleapis.com") !== -1));
+    // }
+    //console.log('imgUrls', imgUrls)
+    //if (imgUrls.length === 0) { return console.log('could not render google maps')}
+    //console.log('saving to ' + url);
+    fs.writeFileSync(url, JSON.stringify(imgUrls.slice(0, 6)));
+
+    //await browser.close();
+    //get apt details
+
+
+
+    return imgUrls;
+}
+
+// Helper function to delay the script execution for `ms` milliseconds
+async function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(async () => {
+    let city_name = process.argv[2]
+    const urls = JSON.parse(fs.readFileSync(`data/airbnb/apt/${city_name}`))
+    console.log(urls, process.argv[2])
+    for (let idx = 0; idx < urls.length; idx++) {
+        const url = urls[idx];
+        const task = getImgUrl(url, idx); // Replace 'link' with the appropriate key for your URLs
+        await task; // If you wish to run these tasks concurrently, you can collect them in an array and use `Promise.all()`
+    }
+})();
---
+++ vite-project/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Map from './Map.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
+/* global window */
+
+// export function renderToDom(container) {
+//   createRoot(container).render(<App />);
+// }
+
+//renderToDom(document.querySelector('#map'))
